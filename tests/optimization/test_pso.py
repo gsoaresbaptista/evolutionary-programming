e84c981a1b2ec8c6ebd0827d79bdf792378f1a8a
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-10-24 23:07:18 -0300

    ✅ tests: add pso tests

---
+++ tests/optimization/test_pso.py
@@ -0,0 +1,17 @@
+from evolutionary_programming.optimization import ParticleSwarm
+from evolutionary_programming.objective_function import RastriginFunction
+
+
+def test_pso_creation():
+    assert ParticleSwarm(1, 1, [-1], [1])
+
+
+def test_pso_optimization():
+    dims = 5
+    particles = 20
+    min_bounds = [-5.2 for _ in range(dims)]
+    max_bounds = [5.2 for _ in range(dims)]
+    pso = ParticleSwarm(particles, dims, min_bounds, max_bounds)
+    fn = RastriginFunction(dims)
+    pso.optimize(1, fn)
+    assert pso.best_value < 201.7664509
