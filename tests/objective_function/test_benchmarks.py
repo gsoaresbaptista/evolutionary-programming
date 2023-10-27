ada1544cb92b9fc10e6b92e4c012fa35b0957bec
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-10-27 14:56:37 -0300

    ✅ tests: update benchmarks tests

--- tests/objective_function/test_benchmarks.py
+++ tests/objective_function/test_benchmarks.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from evolutionary_programming.objective_function import (
     RastriginFunction,
@@ -5,20 +6,18 @@ from evolutionary_programming.objective_function import (
 )
 
 
-def test_rastrigin_function_type():
-    fn = RastriginFunction(3)
-    assert isinstance(fn, BaseFunction)
+@pytest.fixture
+def rastrigin_fn() -> BaseFunction:
+    return RastriginFunction(2)
 
 
-def test_rastrigin_function_creation():
-    assert RastriginFunction(3)
+def test_rastrigin_function_type(rastrigin_fn):
+    assert isinstance(rastrigin_fn, BaseFunction)
 
 
-def test_rastrigin_function_evaluation_low_value():
-    fn = RastriginFunction(3)
-    assert fn.evaluate(np.array([0, 0, 0], dtype='d')) == 0.0
+def test_rastrigin_function_evaluation_low_value(rastrigin_fn):
+    assert rastrigin_fn.evaluate(np.array([0, 0, 0], dtype='d')) == 0.0
 
 
-def test_rastrigin_function_evaluation_high_value():
-    fn = RastriginFunction(2)
-    assert fn.evaluate(np.array([4.5, 4.5])) == 80.5
+def test_rastrigin_function_evaluation_high_value(rastrigin_fn):
+    assert rastrigin_fn.evaluate(np.array([4.5, 4.5])) == 80.5
